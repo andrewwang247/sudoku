f261c532f90d0a4bb645685b119f3952488f5127
siweiw <siweiw@umich.edu>
2019-12-06 13:18:58 -0500

    Added Timing

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,6 +1,8 @@
 #include "grid.h"
 #include <exception>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
 	ios_base::sync_with_stdio( false );
@@ -8,7 +10,11 @@ int main() {
 	while ( cin ) {
 		Grid sudoku ( cin );
 		cout << "Input Puzzle:\n" << sudoku;
+		const auto start_time = high_resolution_clock::now();
 		sudoku.solve();
+		const auto end_time = high_resolution_clock::now();
 		cout << "Computer Solution:\n" << sudoku;
+		const auto duration = chrono::duration_cast<microseconds>(end_time - start_time).count();
+		cout << "Time to solve: " << duration << " microseconds.\n";
 	}
 }
